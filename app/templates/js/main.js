d10c7baa9e2abe9d85b5a0ac9d0615813e3c5a4d
baozi <420129823@qq.com>
2016-01-30 15:06:57 +0800

    wharf finished

--- app/templates/js/main.js
+++ app/templates/js/main.js
@@ -66,6 +66,49 @@ $(document).ready(function(){
   // });
   
   jQuery(".txtScroll-top").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"top",autoPlay:true,vis:15});
+
+  $(".pic1").mouseover(function(){
+    $(".pic1").attr("src","../main/picture/cut2_change_06.png");
+  });
+  $(".pic1").mouseout(function(){
+    $(".pic1").attr("src","../main/picture/cut2_09.png");
+  });
+  $(".pic2").mouseover(function(){
+    $(".pic2").attr("src","../main/picture/cut2_change_06.png");
+  });
+  $(".pic2").mouseout(function(){
+    $(".pic2").attr("src","../main/picture/cut2_09.png");
+  });
+  $(".pic3").mouseover(function(){
+    $(".pic3").attr("src","../main/picture/cut2_change_06.png");
+  });
+  $(".pic3").mouseout(function(){
+    $(".pic3").attr("src","../main/picture/cut2_09.png");
+  });
+  $(".pic4").mouseover(function(){
+    $(".pic4").attr("src","../main/picture/cut2_change_06.png");
+  });
+  $(".pic4").mouseout(function(){
+    $(".pic4").attr("src","../main/picture/cut2_09.png");
+  });
+  $(".pic5").mouseover(function(){
+    $(".pic5").attr("src","../main/picture/cut2_change_06.png");
+  });
+  $(".pic5").mouseout(function(){
+    $(".pic5").attr("src","../main/picture/cut2_09.png");
+  });
+  $(".pic6").mouseover(function(){
+    $(".pic6").attr("src","../main/picture/cut2_change_06.png");
+  });
+  $(".pic6").mouseout(function(){
+    $(".pic6").attr("src","../main/picture/cut2_09.png");
+  });
+  $(".pic7").mouseover(function(){
+    $(".pic7").attr("src","../main/picture/cut2_change_06.png");
+  });
+  $(".pic7").mouseout(function(){
+    $(".pic7").attr("src","../main/picture/cut2_09.png");
+  });
 });
 
 
