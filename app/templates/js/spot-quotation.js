1a8ea8c9fb3d25e8d1a46ec44f182511019caa6f
baozi <420129823@qq.com>
2016-03-10 20:38:12 +0800

    物流行情js代码优化

--- app/templates/js/spot-quotation.js
+++ app/templates/js/spot-quotation.js
@@ -1,67 +1,21 @@
 $(document).ready(function(){
-	$(".meishi-box").mouseover(function(){
+	$(".top-class li").mouseover(function(){
 		$(this).css("color","E4A15D");
-	});
-	$(".meishi-box").mouseout(function(){
+		$(this).css("background-color","rgb(239,239,239)");
+	}).mouseout(function(){
 		$(this).css("color","black");
-	});
-	$(".meishi-quotation").click(function(){
-		$(this).css("border-bottom","none");
-		$(this).css("background-color","white");
-		$(".meishi-know").css("border-bottom","1px solid");
-		$(".meishi-storage").css("border-bottom","1px solid");
-		$(".meishi-index").css("border-bottom","1px solid");
-		$(".meishi-know").css("background-color","rgb(216,216,216)");
-		$(".meishi-storage").css("background-color","rgb(216,216,216)");
-		$(".meishi-index").css("background-color","rgb(216,216,216)");
-		$(".information-box-one").css("display","block");
-		$(".information-box-two").css("display","none");
-		$(".information-box-three").css("display","none");
-		$(".information-box-four").css("display","none");
-	});
-	$(".meishi-know").click(function(){
-		$(this).css("border-bottom","none");
-		$(this).css("background-color","white");
-		$(".meishi-quotation").css("border-bottom","1px solid");
-		$(".meishi-storage").css("border-bottom","1px solid");
-		$(".meishi-index").css("border-bottom","1px solid");
-		$(".meishi-quotation").css("background-color","rgb(216,216,216)");
-		$(".meishi-storage").css("background-color","rgb(216,216,216)");
-		$(".meishi-index").css("background-color","rgb(216,216,216)");
-		$(".information-box-one").css("display","none");
-		$(".information-box-two").css("display","block");
-		$(".information-box-three").css("display","none");
-		$(".information-box-four").css("display","none");
-	});
-	$(".meishi-storage").click(function(){
-		$(this).css("border-bottom","none");
-		$(this).css("background-color","white");
-		$(".meishi-know").css("border-bottom","1px solid");
-		$(".meishi-quotation").css("border-bottom","1px solid");
-		$(".meishi-index").css("border-bottom","1px solid");
-		$(".meishi-know").css("background-color","rgb(216,216,216)");
-		$(".meishi-quotation").css("background-color","rgb(216,216,216)");
-		$(".meishi-index").css("background-color","rgb(216,216,216)");
-		$(".information-box-one").css("display","none");
-		$(".information-box-two").css("display","none");
-		$(".information-box-three").css("display","block");
-		$(".information-box-four").css("display","none");
-	});
-	$(".meishi-index").click(function(){
-		$(this).css("border-bottom","none");
-		$(this).css("background-color","white");
-		$(".meishi-know").css("border-bottom","1px solid");
-		$(".meishi-storage").css("border-bottom","1px solid");
-		$(".meishi-quotation").css("border-bottom","1px solid");
-		$(".meishi-know").css("background-color","rgb(216,216,216)");
-		$(".meishi-storage").css("background-color","rgb(216,216,216)");
-		$(".meishi-quotation").css("background-color","rgb(216,216,216)");
-		$(".information-box-one").css("display","none");
-		$(".information-box-two").css("display","none");
-		$(".information-box-three").css("display","none");
-		$(".information-box-four").css("display","block");
+		$(this).css("background-color","rgb(216,216,216)");
 	});
 
+	$(".top-class li").each(function(index){
+		$(this).click(function(){
+			$("li").removeClass("active");
+			$(this).addClass("active");
+			$(".information-box").removeClass("show-box");
+			$(".information-box").eq(index).addClass("show-box");
+		});
+	});
+	
 	$("#img-one").click(function(){
 		$("#img-one").attr("src","../spot-quotation/picture/cut_change_10.png");
 		$("#img-two").attr("src","../spot-quotation/picture/cut_07.png");
