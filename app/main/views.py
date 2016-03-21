b2a206c24ed789e6b538d2e120b21b0b0ee4874f
FacatZ <475548269@qq.com>
2016-03-21 21:59:41 +0800

    update purchase page

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @main.route('/')
 def index():
 	return render_template('main.html')
+
+@main.route('/purchase')
+def purchase():
+	return render_template('purchase.html')
