7575800e231f2484ba454c6c4a02fc0a0940f8c1
FacatZ <475548269@qq.com>
2016-03-21 22:23:00 +0800

    merge all

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,26 @@ from flask import render_template
 
 @main.route('/')
 def index():
-	return render_template('main.html')
+	activeid = 0
+	return render_template('main.html', activeid=activeid)
+
+@main.route('/stock')
+def stock():
+	activeid = 1
+	return render_template('stock.html', activeid=activeid)
 
 @main.route('/purchase')
 def purchase():
-	return render_template('purchase.html')
+	activeid = 2
+	return render_template('purchase.html', activeid=activeid)
+
+
+@main.route('/logistics')
+def logistics():
+	activeid = 3
+	return render_template('logistics.html', activeid=activeid)
+
+@main.route('/spot_quotation')
+def spot_quotation():
+	activeid = 4
+	return render_template('spot-quotation.html', activeid=activeid)
